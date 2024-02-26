ace3e8514a77095157c7046ad27fa5a5e2906471
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-26 15:58:54 -0800

    initial commit - ready for testing

---
+++ Bakery/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Bakery.Models;
+
+namespace Bakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      Bread bread = new Bread();
+      Pastry pastry = new Pastry();
+    }
+  }
+}
